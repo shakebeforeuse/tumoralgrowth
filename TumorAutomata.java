eaff9749f37373a657b991a93c858ed863c7a84f
Manuel F <shakebeforeuse@gmail.com>
2016-04-10 16:15:58 +0200

    Borrado Estado e includo en TumorAutomata

--- TumorAutomata.java
+++ TumorAutomata.java
@@ -5,6 +5,37 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.Random;
 
+enum Estado
+{
+	MUERTA, LATENTE, VIVA, NUEVA, MIGRADA;
+	
+	public static Estado fromInteger(int i)
+	{
+		Estado e = MUERTA;
+		
+		switch (i)
+		{
+			case 1:
+				e = LATENTE;
+				break;
+			
+			case 2:
+				e = VIVA;
+				break;
+			
+			case 3:
+				e = NUEVA;
+				break;
+				
+			case 4:
+				e = MIGRADA;
+				break;
+		}
+		
+		return e;
+	}
+}
+
 public class TumorAutomata implements Runnable
 {
 	static public double ps;
