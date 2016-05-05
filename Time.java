689b12182fff3aaa90fd9509a88dc9f9534538db
Manuel F <shakebeforeuse@gmail.com>
2016-05-05 23:17:03 +0200

    Added Time and bash script to obtain data

---
+++ Time.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Time
+{
+	public static void main(String[] args) throws Exception
+	{
+		if (args.length == 3)
+		{
+			int size  = Integer.parseInt(args[0]);
+			int steps = Integer.parseInt(args[1]);
+			int tasks = Integer.parseInt(args[2]);
+			
+			tasks = tasks == 1? 0:tasks;
+			
+			TumorAutomaton tumor = new TumorAutomaton(size);
+			
+			tumor.cellState(size/2, size/2, TumorAutomaton.ALIVE);
+			
+			double tic = System.nanoTime();
+			tumor.threads(tasks);
+			tumor.execute(steps);
+			double toc = System.nanoTime();
+			System.out.print((toc-tic)*1e-9);
+			
+			tumor.shutdown();
+		}
+		else
+			System.out.println("Usage: java Time <size> <steps> <tasks>");
+	}
+}
